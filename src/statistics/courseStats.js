1fe3a90e7943d594ee63a820b1811105c6ed8b62
unknown <hassanmohammad922@gmail.com>
2022-06-27 03:08:24 +0300

    Fixed Programme stats bug

--- src/statistics/courseStats.js
+++ src/statistics/courseStats.js
@@ -1,5 +1,5 @@
 import { HorizontalBar } from "react-chartjs-2";
-import React, { useEffect, useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import axios from "axios";
 import {baseUrl} from '../api/busa-api.js'
 import { Card, CardBody } from "@windmill/react-ui";
@@ -18,7 +18,7 @@ const options = {
 };
 
 const CourseStats = () => {
-  useEffect(() => {
+  useLayoutEffect(() => {
     axios.get(`${baseUrl.baseUrl}/courseStats`).then((res) => {
       setCourse(res.data);
       console.log(res);
@@ -39,8 +39,10 @@ const CourseStats = () => {
       "BCom(Procurement and Supply Chain Management)",
       "Bsc Accounting",
       "Bsc Accounting and Finance",
+      "BA Integreated Business Studies (Accounting)",
+      "BA Integreated Business Studies (Management)",
       "BA Management",
-      "Diploma in Business Studies",
+      "Diploma in Business Studies"
     ],
     datasets: [
       {
@@ -56,6 +58,7 @@ const CourseStats = () => {
           result[7],
           result[8],
           result[9],
+          result[10],
         ],
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
@@ -69,7 +72,6 @@ const CourseStats = () => {
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
@@ -78,6 +80,11 @@ const CourseStats = () => {
           "rgba(75, 192, 192, 1)",
           "rgba(153, 102, 255, 1)",
           "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
         ],
         borderWidth: 1,
       },
